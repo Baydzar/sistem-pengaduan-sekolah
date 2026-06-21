eff5d443252a7bacf1f8a495b6ded6c2a30eac56
Ijarudin <mechanicforklift517@gmail.com>
2026-06-21 17:52:25 +0700

    feat: implement pengaduan schema model with relations to user

---
+++ backend/models/Pengaduan.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const PengaduanSchema = new mongoose.Schema({
+  // Relasi ke model User
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: [true, 'User ID wajib diisi']
+  },
+  judul: {
+    type: String,
+    required: [true, 'Judul pengaduan tidak boleh kosong'],
+    trim: true
+  },
+  kategori: {
+    type: String,
+    required: [true, 'Kategori wajib dipilih'],
+    enum: ['Fasilitas', 'Kurikulum', 'Kesiswaan', 'Lainnya']
+  },
+  isi: {
+    type: String,
+    required: [true, 'Isi pengaduan tidak boleh kosong']
+  },
+  status: {
+    type: String,
+    enum: ['Pending', 'Proses', 'Selesai'],
+    default: 'Pending'
+  }
+}, {
+  timestamps: true // Otomatis membuat createdAt dan updatedAt
+});
+
+module.exports = mongoose.model('Pengaduan', PengaduanSchema);
