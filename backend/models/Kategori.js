efda115f2b7cbcd15f711a64b8dac5d84bb344b8
Baydzar <mechanicforklift517@gmail.com>
2026-07-01 11:02:01 +0700

    feat(category): implement complaint category management

---
+++ backend/models/Kategori.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const kategoriSchema = new mongoose.Schema(
+  {
+    nama: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+
+    deskripsi: {
+      type: String,
+      default: "",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Kategori", kategoriSchema);
