65fb9b959d7b13007067298d49e8d59e4dbf93ed
Baydzar <mechanicforklift517@gmail.com>
2026-07-01 11:07:45 +0700

    feat(auth): add jwt authentication middleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,34 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+    try {
+
+        const header = req.headers.authorization;
+
+        if (!header) {
+            return res.status(401).json({
+                success: false,
+                message: "Token tidak ditemukan"
+            });
+        }
+
+        const token = header.split(" ")[1];
+
+        const decoded = jwt.verify(
+            token,
+            process.env.JWT_SECRET
+        );
+
+        req.user = decoded;
+
+        next();
+
+    } catch (err) {
+
+        return res.status(401).json({
+            success: false,
+            message: "Token tidak valid"
+        });
+
+    }
+};
